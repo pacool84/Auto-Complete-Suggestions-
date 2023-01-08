f0749f7d6a3c7233dccc4a03981d773575171cf8
Pacool84 <flopez_1384@hotmail.com>
2023-01-08 13:19:59 -0600

    [ADD] Last changes tu upload the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  PlacesAutocomplete,
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+import "./App.css";
 
 function App() {
+  const [address, setAddress] = React.useState("");
+  const handleSelect = async (value) => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PlacesAutocomplete
+        value={address}
+        onChange={setAddress}
+        onSelect={handleSelect}
+      >
+        {" "}
+        {() => <div>eeeyyyyy</div>}{" "}
+      </PlacesAutocomplete>
     </div>
   );
 }
